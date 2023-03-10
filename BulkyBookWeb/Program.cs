0a1212cb10917cf7891d62db57ea75a515930cc7
kendrick chukwuka <kendrickchibueze@gmail.com>
2023-03-10 23:52:40 +0100

    Add project files.

---
+++ BulkyBookWeb/Program.cs
@@ -0,0 +1,56 @@
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+          
+
+            builder.Services.AddDbContext<ApplicationDbContext>(opts =>
+            {
+                // this will only work if there's a section called ConnectionStrings on the appSettings
+                // var defaultConn = builder.Configuration.GetConnectionString("DefaultConn");
+
+                var defaultConn = builder.Configuration.GetSection("ConnectionString")["DefaultConnection"];
+
+                opts.UseSqlServer(defaultConn);
+
+            });
+
+
+
+            //To get runtime compilation
+            //builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
